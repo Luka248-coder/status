645dab51d0da59f2d828d9be9b53c0f409161660
Luka248-coder <blukab1.140@icloud.com>
2025-01-11 00:51:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Streame les convocations...", "Streame Les convocations..."];
+const statusMessages = ["Streame les inscriptions...", "Streame les inscriptions..."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
