f0ff94876062b99c197dcbcb358cbd45019be3ef
Luka248-coder <blukab1.140@icloud.com>
2025-02-04 19:07:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Je gère vos inscriptions !", "Je gère vos inscriptions !"];
+const statusMessages = ["Angel + Riley = ∞", "Angel + Riley = ∞"];
 const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
